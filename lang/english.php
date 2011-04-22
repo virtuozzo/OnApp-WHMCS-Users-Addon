3c810e4a0f6c23309741b5731606396139f2ac5c
Lev Bartashevsky <lev@onapp.com>
2011-04-22 13:02:54 +0300

    add new items

--- lang/english.php
+++ lang/english.php
@@ -1,5 +1,5 @@
 <?php
-
+// common
 $_ADDONLANG[ 'SearchFilter' ] = 'Search/Filter';
 $_ADDONLANG[ 'Name' ] = 'Name';
 $_ADDONLANG[ 'FirstName' ] = 'First Name';
@@ -15,13 +15,16 @@ $_ADDONLANG[ 'Next' ] = 'Next';
 $_ADDONLANG[ 'Jump' ] = 'Jump to';
 $_ADDONLANG[ 'Found' ] = 'Record(s) Found';
 $_ADDONLANG[ 'Of' ] = 'of';
-$_ADDONLANG[ 'Unmap' ] = 'unmap';
 $_ADDONLANG[ 'Map' ] = 'map';
 $_ADDONLANG[ 'View' ] = 'view';
 $_ADDONLANG[ 'User' ] = 'User';
 $_ADDONLANG[ 'Status' ] = 'Status';
 $_ADDONLANG[ 'ID' ] = 'ID';
+$_ADDONLANG[ 'Resources' ] = 'Resources';
+$_ADDONLANG[ 'Billing' ] = 'Billing';
+$_ADDONLANG[ 'Information' ] = 'Information';
 
+// stat
 $_ADDONLANG[ 'TotalAmount' ] = 'Total Amount';
 $_ADDONLANG[ 'PaymentAmount' ] = 'Payment Amount';
 $_ADDONLANG[ 'OutstandingAmount' ] = 'Outstanding Amount';
@@ -31,6 +34,17 @@ $_ADDONLANG[ 'DiskSpaceAvailable' ] = 'Disk Space Available';
 $_ADDONLANG[ 'UsedDiskSize' ] = 'Used Disk Size';
 $_ADDONLANG[ 'UsedCPUs' ] = 'Used CPUs';
 $_ADDONLANG[ 'UsedCPUShares' ] = 'Used CPU Shares';
+$_ADDONLANG[ 'Activate' ] = 'Activate';
+$_ADDONLANG[ 'Unmap' ] = 'Unmap';
+
+// messages
+$_ADDONLANG[ 'ActivatedSuccessfully' ] = 'User was activated successfully';
+$_ADDONLANG[ 'ActivatedError' ] = 'There was error during user activation: ';
 $_ADDONLANG[ 'ID' ] = 'ID';
 $_ADDONLANG[ 'ID' ] = 'ID';
 $_ADDONLANG[ 'ID' ] = 'ID';
+$_ADDONLANG[ 'ID' ] = 'ID';
+$_ADDONLANG[ 'ID' ] = 'ID';
+$_ADDONLANG[ 'ID' ] = 'ID';
+// JavaScript
+$_ADDONLANG_JS[ 'UnmapAlert' ] = 'Do you really want to unmap user?';
