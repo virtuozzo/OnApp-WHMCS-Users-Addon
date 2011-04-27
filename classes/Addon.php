0345efdaf5563cb25809ca0044ce56bf53b369cf
Lev Bartashevsky <lev@onapp.com>
2011-04-27 16:56:09 +0300

    add new functionality

--- classes/Addon.php
+++ classes/Addon.php
@@ -28,6 +28,18 @@ class OnApp_Users_Addon {
         elseif( isset( $_GET[ 'activate' ] ) ) {
             $this->activate( );
         }
+        elseif( isset( $_GET[ 'syncdata' ] ) ) {
+            $this->syncData( );
+        }
+        elseif( isset( $_GET[ 'syncauth' ] ) ) {
+            $this->syncAuth( );
+        }
+        elseif( isset( $_GET[ 'suspend' ] ) ) {
+            $this->suspend( );
+        }
+        elseif( isset( $_GET[ 'signin' ] ) ) {
+            $this->signin( );
+        }
 
         if( !isset( $_GET[ 'page' ] ) ) {
             $_GET[ 'page' ] = 1;
@@ -49,34 +61,11 @@ class OnApp_Users_Addon {
     }
 
     public function getUsersFromOnApp( ) {
-        var_dump( __METHOD__ );
-
         $server = $this->servers[ $_GET[ 'server_id' ] ];
 
-        /**
-         * test staff
-         * delete when finish and uncomment block below
-         */
-        if( file_exists( $file = __DIR__ . '/' . $_GET[ 'server_id' ] . '.srv' ) ) {
-            var_dump( 'CACHE' );
-            $required_path = __DIR__ . '/../wrapper/';
-            require_once $required_path . 'User.php';
-            $users = unserialize( file_get_contents( $file ) );
-        }
-        else {
-            $pass = decrypt( $server[ 'password' ] );
-            $class = $this->getOnAppObject( 'ONAPP_User', $server[ 'ipaddress' ], $server[ 'username' ], $pass );
-            $users = $class->getList( );
-
-            file_put_contents( $file, serialize( $users ) );
-        }
-
-        // todo uncomment block below & remove block above
-        /* uncomment when finish testing
         $pass = decrypt( $server[ 'password' ] );
         $class = $this->getOnAppObject( 'ONAPP_User', $server[ 'ipaddress' ], $server[ 'username' ], $pass );
         $users = $class->getList( );
-        */
 
         $sql = 'SELECT `onapp_user_id` FROM `tblonappclients` WHERE `server_id` = ' . $_GET[ 'server_id' ];
         $res = full_query( $sql );
@@ -146,8 +135,6 @@ class OnApp_Users_Addon {
     }
 
     public function getUsersFromWHMCS( $id = false ) {
-        var_dump( __METHOD__ );
-
         if( $id ) {
             $sql = 'SELECT whmcs.* FROM `tblclients` AS whmcs WHERE whmcs.`id` = ' . $id . ' LIMIT 1';
 
@@ -190,8 +177,6 @@ class OnApp_Users_Addon {
     }
 
     public function getUserData( ) {
-        var_dump( __METHOD__ );
-
         $sql = 'SELECT whmcs.*, onapp.email as mail, onapp.client_id, onapp.server_id, onapp.onapp_user_id'
                . ' FROM `tblclients` AS whmcs LEFT JOIN `tblonappclients` AS onapp ON whmcs.`id` = onapp.`client_id`'
                . ' WHERE onapp.`server_id` = ' . $_GET[ 'server_id' ] . ' AND whmcs.`id` = ' . $_GET[ 'whmcs_user_id' ]
@@ -255,7 +240,9 @@ class OnApp_Users_Addon {
             '&unmap',
             '&domap',
             '&activate',
-            '&info'
+            '&syncdata',
+            '&syncauth',
+            '&info',
         );
         foreach( $params as $param ) {
             if( strpos( $_SERVER[ 'REQUEST_URI' ], $param ) ) {
@@ -283,44 +270,218 @@ class OnApp_Users_Addon {
         $headers = array( 'Accept: application/json', 'Content-type: application/json', 'Content-Length: ' . strlen( $data ) );
 
         include_once 'CURL.php';
-
         $curl = new CURL( );
-
         $curl->addOption( CURLOPT_USERPWD, $server[ 'username' ] . ':' . $pass );
-        $curl->addOption( CURLOPT_POSTFIELDS, $data );
         $curl->addOption( CURLOPT_HTTPHEADER, $headers );
+        $curl->addOption( CURLOPT_POSTFIELDS, $data );
+        $curl->addOption( CURLOPT_HEADER, true );
 
-        $contents = $curl->put( 'http://' . $server[ 'ipaddress' ] . '/users/' . $_GET[ 'onapp_user_id' ] . '.json' );
+        $content = $curl->put( 'http://' . $server[ 'ipaddress' ] . '/users/' . $_GET[ 'onapp_user_id' ] . '.json' );
 
-        $res = insert_query( 'tblonappclients', array(
-            'server_id' => $_GET[ 'server_id' ],
-            'client_id' => $_GET[ 'whmcs_user_id' ],
-            'onapp_user_id' => $_GET[ 'onapp_user_id' ],
-            'password' => encrypt( $whmcsuser[ 'password' ] ),
-            'email' => $whmcsuser[ 'email' ]
-        ) );
+        $this->smarty->assign( 'msg', true );
+        if( $curl->getRequestInfo( 'http_code' ) == 200 ) {
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'MapedSuccessfully' ] );
+            $this->smarty->assign( 'msg_ok', true );
 
-        $error = mysql_error( );
+            insert_query( 'tblonappclients', array(
+                'server_id' => $_GET[ 'server_id' ],
+                'client_id' => $_GET[ 'whmcs_user_id' ],
+                'onapp_user_id' => $_GET[ 'onapp_user_id' ],
+                'password' => encrypt( $whmcsuser[ 'password' ] ),
+                'email' => $whmcsuser[ 'email' ]
+            ) );
+        }
+        else {
+            if( is_null( $tmp = json_decode( $content ) ) ) {
+                $msg = $content;
+            }
+            else {
+                $msg = $tmp->error;
+            }
 
-        if( empty( $error ) ) {
-            $sql = 'UPDATE `mod_onapp_users` SET `whmcs_user_id` = ' . $_GET[ 'whmcs_user_id' ] . ' WHERE '
-                   . ' `onapp_user_id` = ' . $_GET[ 'onapp_user_id' ] . ' AND `onapp_user_server_id` = '
-                   . $_GET[ 'server_id' ];
-
-            mysql_query( $sql );
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'MapedError' ] . $msg );
+            $this->smarty->assign( 'msg_ok', false );
         }
     }
 
     private function unmap( ) {
-        var_dump( __METHOD__ );
-
         $sql = 'DELETE FROM `tblonappclients` WHERE `client_id` = ' . $_GET[ 'whmcs_user_id' ] . ' AND `onapp_user_id`'
                . ' = ' . $_GET[ 'onapp_user_id' ] . ' AND `server_id` = ' . $_GET[ 'server_id' ];
         mysql_query( $sql );
+
+        $error = mysql_error( );
+        $this->smarty->assign( 'msg', true );
+        if( empty( $error ) ) {
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'UnmapedSuccessfully' ] );
+            $this->smarty->assign( 'msg_ok', true );
+        }
+        else {
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'UnmapedError' ] . $error );
+            $this->smarty->assign( 'msg_ok', false );
+
+            $this->cleanParams( );
+            $_GET[ 'info' ] = true;
+            $this->smarty->assign( 'info', true );
+        }
     }
 
     private function activate( ) {
-        var_dump( __METHOD__ );
+        $sql = 'SELECT `id`, `name`, `ipaddress`, `hostname`, `username`, `password`'
+               . ' FROM `tblservers` WHERE `id` = ' . $_GET[ 'server_id' ];
+        $res = full_query( $sql );
+        $server = mysql_fetch_assoc( $res );
+        $pass = decrypt( $server[ 'password' ] );
+
+        $headers = array( 'Accept: application/json', 'Content-type: application/json' );
+
+        include_once 'CURL.php';
+        $curl = new CURL( );
+        $curl->addOption( CURLOPT_USERPWD, $server[ 'username' ] . ':' . $pass );
+        $curl->addOption( CURLOPT_HTTPHEADER, $headers );
+        $curl->addOption( CURLOPT_HEADER, true );
+
+        $url = 'http://' . $server[ 'ipaddress' ] . '/users/' . $_GET[ 'onapp_user_id' ] . '/activate_user.json';
+        $content = $curl->get( $url );
+
+        $this->smarty->assign( 'msg', true );
+        if( $curl->getRequestInfo( 'http_code' ) == 200 ) {
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'ActivatedSuccessfully' ] );
+            $this->smarty->assign( 'msg_ok', true );
+        }
+        else {
+            if( is_null( $tmp = json_decode( $content ) ) ) {
+                $msg = $content;
+            }
+            else {
+                $msg = $tmp->error;
+            }
+
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'ActivatedError' ] . $msg );
+            $this->smarty->assign( 'msg_ok', false );
+        }
+
+        $this->cleanParams( );
+        $_GET[ 'info' ] = true;
+        $this->smarty->assign( 'info', true );
+    }
+
+    private function suspend( ) {
+        $sql = 'SELECT `id`, `name`, `ipaddress`, `hostname`, `username`, `password`'
+               . ' FROM `tblservers` WHERE `id` = ' . $_GET[ 'server_id' ];
+        $res = full_query( $sql );
+        $server = mysql_fetch_assoc( $res );
+        $pass = decrypt( $server[ 'password' ] );
+
+        $headers = array( 'Accept: application/json', 'Content-type: application/json' );
+
+        include_once 'CURL.php';
+        $curl = new CURL( );
+        $curl->addOption( CURLOPT_USERPWD, $server[ 'username' ] . ':' . $pass );
+        $curl->addOption( CURLOPT_HTTPHEADER, $headers );
+        $curl->addOption( CURLOPT_HEADER, true );
+
+        $url = 'http://' . $server[ 'ipaddress' ] . '/users/' . $_GET[ 'onapp_user_id' ] . '/suspend.json';
+        $content = $curl->get( $url );
+
+        $this->smarty->assign( 'msg', true );
+        if( $curl->getRequestInfo( 'http_code' ) == 200 ) {
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'SuspendSuccessfully' ] );
+            $this->smarty->assign( 'msg_ok', true );
+        }
+        else {
+            if( is_null( $tmp = json_decode( $content ) ) ) {
+                $msg = $content;
+            }
+            else {
+                $msg = $tmp->error;
+            }
+
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'SuspendError' ] . $msg );
+            $this->smarty->assign( 'msg_ok', false );
+        }
+
+        $this->cleanParams( );
+        $_GET[ 'info' ] = true;
+        $this->smarty->assign( 'info', true );
+    }
+
+    private function syncData( ) {
+        $sql = 'SELECT `id`, `firstname`, `lastname`, `email`, `password` FROM `tblclients`'
+               . ' WHERE `id` = ' . $_GET[ 'whmcs_user_id' ];
+        $res = full_query( $sql );
+        $whmcsuser = mysql_fetch_assoc( $res );
+
+        $sql = 'SELECT `id`, `name`, `ipaddress`, `hostname`, `username`, `password`'
+               . ' FROM `tblservers` WHERE `id` = ' . $_GET[ 'server_id' ];
+        $res = full_query( $sql );
+        $server = mysql_fetch_assoc( $res );
+        $pass = decrypt( $server[ 'password' ] );
+
+        $data[ 'first_name' ] = $whmcsuser[ 'firstname' ];
+        $data[ 'last_name' ] = $whmcsuser[ 'lastname' ];
+        $data[ 'email' ] = $whmcsuser[ 'email' ];
+
+        $data = json_encode( array( 'user' => $data ) );
+        $headers = array( 'Accept: application/json', 'Content-type: application/json', 'Content-Length: ' . strlen( $data ) );
+
+        include_once 'CURL.php';
+        $curl = new CURL( );
+        $curl->addOption( CURLOPT_USERPWD, $server[ 'username' ] . ':' . $pass );
+        $curl->addOption( CURLOPT_HTTPHEADER, $headers );
+        $curl->addOption( CURLOPT_POSTFIELDS, $data );
+        $curl->addOption( CURLOPT_HEADER, true );
+
+        $url = 'http://' . $server[ 'ipaddress' ] . '/users/' . $_GET[ 'onapp_user_id' ] . '.json';
+        $content = $curl->put( $url );
+
+        $this->smarty->assign( 'msg', true );
+        if( $curl->getRequestInfo( 'http_code' ) == 200 ) {
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'DataSyncedSuccessfully' ] );
+            $this->smarty->assign( 'msg_ok', true );
+        }
+        else {
+            if( is_null( $tmp = json_decode( $content ) ) ) {
+                $msg = $content;
+            }
+            else {
+                $msg = '';
+                foreach( $tmp as $k => $v ) {
+                    $msg .= $k . ' ' . $v . '<br/>';
+                }
+            }
+
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'DataSyncedError' ] . $msg );
+            $this->smarty->assign( 'msg_ok', false );
+        }
+
+        $this->cleanParams( );
+        $_GET[ 'info' ] = true;
+        $this->smarty->assign( 'info', true );
+    }
+
+    private function signin( ) {
+        $sql = 'SELECT `password`, `email` FROM tblonappclients '
+               . 'WHERE `onapp_user_id` = ' . $_GET[ 'onapp_user_id' ] . ' AND `server_id` = ' . $_GET[ 'server_id' ]
+               . ' AND `client_id` = ' . $_GET[ 'whmcs_user_id' ];
+        $res = full_query( $sql );
+        $onapp_user = mysql_fetch_assoc( $res );
+
+        $sql = 'SELECT `id`, `name`, `ipaddress`, `hostname`, `username`, `password`'
+               . ' FROM `tblservers` WHERE `id` = ' . $_GET[ 'server_id' ];
+        $res = full_query( $sql );
+        $server = mysql_fetch_assoc( $res );
+
+        $url = urlencode($onapp_user[ 'email' ]) . ':' . decrypt( $onapp_user[ 'password' ] )
+               . '@' . $server[ 'ipaddress' ] . '/users/';
+        header( 'Location: http://' . $url );
+    }
+
+    private function syncAuth( ) {
+        $sql = 'SELECT `password`, `email` FROM tblonappclients '
+               . 'WHERE `onapp_user_id` = ' . $_GET[ 'onapp_user_id' ] . ' AND `server_id` = ' . $_GET[ 'server_id' ]
+               . ' AND `client_id` = ' . $_GET[ 'whmcs_user_id' ];
+        $res = full_query( $sql );
+        $onapp_user = mysql_fetch_assoc( $res );
 
         $sql = 'SELECT `id`, `name`, `ipaddress`, `hostname`, `username`, `password`'
                . ' FROM `tblservers` WHERE `id` = ' . $_GET[ 'server_id' ];
@@ -332,23 +493,66 @@ class OnApp_Users_Addon {
 
         include_once 'CURL.php';
         $curl = new CURL( );
-
-        $curl->addOption( CURLOPT_USERPWD, $server[ 'username' ] . ':' . $pass );
+        $curl->addOption( CURLOPT_USERPWD, $onapp_user[ 'email' ] . ':' . decrypt( $onapp_user[ 'password' ] ) );
         $curl->addOption( CURLOPT_HTTPHEADER, $headers );
         $curl->addOption( CURLOPT_HEADER, true );
 
-        $url = 'http://' . $server[ 'ipaddress' ] . '/users/' . $_GET[ 'onapp_user_id' ] . '/activate_user.json';
+        $url = 'http://' . $server[ 'ipaddress' ] . '/users/' . $_GET[ 'onapp_user_id' ] . '.json';
         $content = $curl->get( $url );
 
         $this->smarty->assign( 'msg', true );
-        if( $curl->getHeadersInfo( 'http_code' ) == 200 ) {
-            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'ActivatedSuccessfully' ] );
-            $this->smarty->assign( 'msg_ok', true );
+        if( $curl->getRequestInfo( 'http_code' ) != 200 ) {
+            $sql = 'SELECT `id`, `name`, `ipaddress`, `hostname`, `username`, `password`'
+                   . ' FROM `tblservers` WHERE `id` = ' . $_GET[ 'server_id' ];
+            $res = full_query( $sql );
+            $server = mysql_fetch_assoc( $res );
+            $pass = decrypt( $server[ 'password' ] );
+
+            $curl = new CURL( );
+            $curl->addOption( CURLOPT_USERPWD, $server[ 'username' ] . ':' . $pass );
+            $curl->addOption( CURLOPT_HTTPHEADER, $headers );
+            $curl->addOption( CURLOPT_HEADER, true );
+
+            $url = 'http://' . $server[ 'ipaddress' ] . '/users/' . $_GET[ 'onapp_user_id' ] . '.json';
+            $content = $curl->get( $url );
+
+            if( $curl->getRequestInfo( 'http_code' ) == 200 ) {
+                $content = json_decode( $content );
+                $user = $content->user;
+
+                $sql = 'SELECT `id`, `firstname`, `lastname`, `email`, `password` FROM `tblclients`'
+                       . ' WHERE `id` = ' . $_GET[ 'whmcs_user_id' ];
+                $res = full_query( $sql );
+                $whmcsuser = mysql_fetch_assoc( $res );
+
+                $sql = 'UPDATE tblonappclients SET `email` = "' . $user->login . '", '
+                       . '`password` = "' . encrypt( $whmcsuser[ 'password' ] ) . '"';
+
+                mysql_query( $sql );
+
+                $data[ 'password' ] = $whmcsuser[ 'password' ];
+                $data[ 'password_confirmation' ] = $whmcsuser[ 'password' ];
+                $data = json_encode( array( 'user' => $data ) );
+
+                $headers = array( 'Accept: application/json', 'Content-type: application/json', 'Content-Length: ' . strlen( $data ) );
+
+                include_once 'CURL.php';
+                $curl = new CURL( );
+                $curl->addOption( CURLOPT_USERPWD, $server[ 'username' ] . ':' . $pass );
+                $curl->addOption( CURLOPT_HTTPHEADER, $headers );
+                $curl->addOption( CURLOPT_POSTFIELDS, $data );
+                $curl->addOption( CURLOPT_HEADER, true );
+
+                $content = $curl->put( 'http://' . $server[ 'ipaddress' ] . '/users/' . $_GET[ 'onapp_user_id' ] . '.json' );
+            }
+            else {
+                $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'AuthSyncedError' ] );
+                $this->smarty->assign( 'msg_ok', false );
+            }
         }
         else {
-            $content = json_decode( $content );
-            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'ActivatedError' ] . $content->error );
-            $this->smarty->assign( 'msg_ok', false );
+            $this->smarty->assign( 'msg_text', $GLOBALS[ '_LANG' ][ 'AuthSyncedSuccessfully' ] );
+            $this->smarty->assign( 'msg_ok', true );
         }
 
         $this->cleanParams( );
@@ -362,14 +566,11 @@ class OnApp_Users_Addon {
             require_once $required_path . 'ONAPP.php';
         }
 
-        var_dump( "$class, $server_ip, $username, $apikey" );
-
         $required_file = str_replace( 'ONAPP_', '', $class ) . '.php';
         require_once $required_path . $required_file;
 
         $obj = new $class;
         $obj->auth( $server_ip, $username, $apikey );
-        $obj->_loger->setDebug( true );
 
         return $obj;
     }
